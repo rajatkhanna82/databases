8118710e9487c04960e3b136e40e4fa2e91940d4
Hack Reactor Students <rajatkhanna82@gmail.com>
2014-05-10 19:00:11 -0700

    Refactor to use ORM

--- ORM_Refactor/orm-example.js
+++ ORM_Refactor/orm-example.js
@@ -3,34 +3,56 @@
  * before running this example. Documentation is at http://sequelizejs.com/
  */
 
-var Sequelize = require("sequelize");
-var sequelize = new Sequelize("chat", "bacon", "pass");
+var exports = exports; // make linter happy
+
+var Sequelize = require('sequelize');
+console.log('sequilizing imported')
+var sequelize = new Sequelize('chat', 'root', '');
 /* TODO this constructor takes the database name, username, then password.
  * Modify the arguments if you need to */
 
 /* first define the data structure by giving property names and datatypes
  * See http://sequelizejs.com for other datatypes you can use besides STRING. */
-var User = sequelize.define('User', {
-  user_name: Sequelize.STRING,
+var User = sequelize.define('users', {
+  name: Sequelize.STRING,
 });
+
+var Room = sequelize.define('rooms', {
+  name: Sequelize.STRING,
+});
+
+var Message = sequelize.define('messages', {
+  text: Sequelize.STRING
+});
+
+Message.belongsTo(Room);
+Message.belongsTo(User);
 
 /* .sync() makes Sequelize create the database table for us if it doesn't
  *  exist already: */
-User.sync().success(function() {
+Room.sync();
+User.sync();
+Message.sync();
+
   /* This callback function is called once sync succeeds. */
 
   // now instantiate an object and save it:
-  var newUser = User.build({user_name: "Jean Valjean"});
-  newUser.save().success(function() {
+  // var newUser = User.build({user_name: 'davis'});
+  // newUser.save().success(function() {
 
-    /* This callback function is called once saving succeeds. */
+  //   /* This callback function is called once saving succeeds. */
 
-    // Retrieve objects from the database:
-    User.findAll({ where: {user_name: "Jean Valjean"} }).success(function(usrs) {
-      // This function is called back with an array of matches.
-      for (var i = 0; i < usrs.length; i++) {
-        console.log(usrs[i].user_name + " exists");
-      }
-    });
-  });
-});
+  //   // Retrieve objects from the database:
+  //   User.findAll({ where: {user_name: 'davis'} }).success(function(usrs) {
+  //     // This function is called back with an array of matches.
+  //     for (var i = 0; i < usrs.length; i++) {
+  //       console.log(usrs[i].user_name + ' exists');
+  //     }
+    // });
+  // });
+
+
+
+exports.User = User;
+exports.Room = Room;
+exports.Message= Message;
