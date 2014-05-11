7259b26bb9285505c9d7e385437ee8e1280b73fa
Hack Reactor Students <rajatkhanna82@gmail.com>
2014-05-10 19:00:28 -0700

    Use ORM instead of raw SQL

--- chatterbox/app.js
+++ chatterbox/app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
-
+var bodyParse = require('body-parser');
 // Use static middleware
+// app.configure(function () {
+app.use(bodyParse.json({type: 'application/json'}));
+app.use(function(req, res, next) {
+  console.log(req.body)
+  next();
+});
 app.use(express.static(__dirname + '/public'));
+// });
 
 // Set the view engine
 var jade = require('jade');
@@ -28,8 +35,52 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+console.log('before sequelizing')
+
+// Sequelize code ===============================================================================
+var Room = require('../ORM_Refactor/orm-example.js').Room;
+console.log('room')
+var User = require('../ORM_Refactor/orm-example.js').User;
+console.log('user')
+var Message = require('../ORM_Refactor/orm-example.js').Message;
+console.log('msg')
+
+
+app.get('/classes/chatterbox', function(req, res) {
+  // get messages from db and put into a {results: messages} format and send it
+  Message.findAll().success(function(messages) {
+    console.log("messages", messages);
+    res.send({results: messages});
+  });
+});
+
+app.post('/classes/chatterbox', function(req, res) {
+  console.log(req.body);
+  Room
+    .findOrCreate({name: req.body.roomname})
+    .success(function(room){
+    console.log('=========================', room.id)
+    User
+      .findOrCreate({name: req.body.username})
+      .success(function(user){
+      Message
+        .create({text: req.body.text, userId: user.id, roomId: room.id})
+        .success(function(){
+          res.end();
+        });
+      });
+    });
+});
+
+
+
+// Pre-database code===========================
 // var messages = [];
 // var idCounter = 1;
+
+
+// SQL (first pass) code =================================================================
+/*
 var db = require('./db-helpers');
 
 app.get('/classes/chatterbox', function(req, res) {
@@ -56,6 +107,7 @@ app.post('/classes/chatterbox', function(req, res) {
     res.end();
   });
 });
+*/
 
 
 
